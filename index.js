7878681e4db3ba40490bb2e230f675afe7bcad49
magas1507 <asmg15.07@gmail.com>
2023-07-26 22:58:32 -0300

    Get: parametros query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 3005;
 
@@ -16,6 +18,86 @@ app.get('/producto', (req, res)=>{
     price: 1000
   });
 })
+
+//Get: parámetros query
+app.get('/products',(req, res)=>{
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    })
+  }
+  res.json(products);
+})
+
+app.get('/products/:id', (req, res)=>{
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name:'Product 2',
+    price: 2000
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId',(req,res)=>{
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  })
+})
+
+// Personas
+const persons = [
+  {
+    id: 1,
+    name:'Rhysan',
+    country:'Corte de la noche',
+    city:'Velaris',
+    age:'500 years old'
+  },
+  {
+    id: 2,
+    name:'Azriel',
+    country:'Corte de la noche',
+    city:'Velaris',
+    age:'350 years old'
+  },
+  {
+    id: 3,
+    name:'Casian',
+    country:'Corte de la noche',
+    city:'Velaris',
+    age:'450 years old'
+  },
+];
+
+app.get('/persons',(req,res)=>{
+  res.json(persons);
+})
+
+app.get('/persons/:personId',(req , res)=>{
+  const { personId } = req.params;
+
+  const getPerson = persons.find((person)=> person.id === Number(personId));
+
+  if (getPerson) {
+
+    res.json(getPerson)
+  } else {
+    res.json({ success: false, message:'person not found'})
+  }
+
+})
+
+
+
 
 app.listen(port, ()=>{
   console.log('Mi port' + port);
